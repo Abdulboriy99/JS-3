5006c3fdf9a1d184ad348f4ba9deb17834fce939
abdulboriy <adb@gmail.com>
2024-12-03 14:05:35 +0500

    JS-3 commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var boss = 'abdulboriy'
+var user = prompt('ismingizni kiriting!')
+if (boss === user ){
+  alert('HELLO BOSS')
+}
+else if(!isNaN(user)){
+  alert('faqat harif kiriting!')
+}
+else{
+alert('HELLO GUEST')
+
+}
